bcfeb949d8b1dad03e0ed29eeb1c8c93fcacedfb
Keith McDonnell <keith@dancingtext.com>
2012-10-02 11:49:58 +0100

    jruby evaluation

--- src/main/groovy/historic_data.groovy
+++
@@ -1,7 +0,0 @@
-IBUtils.forEachNDX10 { gateway, i, symbol ->
-  def stock = new Stock(symbol)
-  gateway.stocks[i] = stock
-
-  gateway.client_socket.reqHistoricalData(i, stock.contract, new Date().format("yyyyMMdd HH:mm:ss"), '3 D', '1 min',
-          'ASK', 1, 1)
-}
