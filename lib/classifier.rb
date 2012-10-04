246c68a94778cf593c8e348d53dc25c80368d9a0
Keith McDonnell <keith@dancingtext.com>
2012-10-04 10:56:51 +0100

    Example is an importanct concept in its own right

--- lib/classifier.rb
+++ lib/classifier.rb
@@ -28,29 +28,3 @@ class Classifier
       Example.new(current_bar, previous_bar).classification
   end
 end
-
-class Example
-  attr_reader :current_bar, :previous_bar
-  
-  def initialize(current_bar, previous_bar)
-    @current_bar, @previous_bar = current_bar, previous_bar
-  end
-  
-  def classification
-    return if previous_bar.empty?
-    return :long if long?
-    return :short if short?
-  end
-  
-  def long?
-    change(:close) >= 0.05 && change(:low) >= -0.02
-  end
-  
-  def short?
-    change(:close) <= -0.05 && change(:high) <= 0.02
-  end
-  
-  def change(ohlc)
-    BigDecimal.new(current_bar[ohlc].to_s) - BigDecimal.new(previous_bar[ohlc].to_s)
-  end
-end
