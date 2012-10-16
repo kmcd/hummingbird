1f44d84266e278956321035c74d83aa6e3d5655b
Keith McDonnell <keith@dancingtext.com>
2012-10-16 14:31:25 +0100

    Cleaned up quick & dirty backtester

--- bin/backtest.rb
+++ bin/backtest.rb
@@ -3,32 +3,30 @@ require 'hummingbird'
 require 'historic_data'
 
 # TODO: take index, components, day range from script args
-# TODO: loaded cached data from YAML if available
+# TODO: load previously retrieved data from yaml
 
-# Fetch data: previous 4 - 2 days 
-previous = 3
-end_day = 2.days.ago.end_of_day.strftime("%Y%m%d %H:%M:%S")
-
-train = HistoricData.new
-train.request 'SQQQ', end_day, previous
-train.request %w[ AAPL MSFT GOOG ORCL INTC AMZN QCOM CSCO CMCSA AMGN ], 
-  end_day, previous
+def request_historic_data(days, previous)
+  ndx10 = %w[ AAPL MSFT GOOG ORCL INTC AMZN QCOM CSCO CMCSA AMGN ]
+  end_day = days.days.ago.end_of_day.strftime("%Y%m%d %H:%M:%S")
   
-# TODO: wait for train.historic_data.keys
-knn = KNearestNeighbours.new Classifer.new(train.historic_data, 'SQQQ').trained_examples
-
-# Fetch data: previous 1 days
-previous = 1
-end_day = 1.days.ago.end_of_day.strftime("%Y%m%d %H:%M:%S")
-
-test = HistoricData.new
-test.request 'SQQQ', end_day, previous
-
-# TODO: wait for test.historic_data.keys
-test.request %w[ AAPL MSFT GOOG ORCL INTC AMZN QCOM CSCO CMCSA AMGN ], 
-  end_day, previous
+  hd = HistoricData.new
+  hd.request 'SQQQ', end_day, previous
+  hd.request ndx10, end_day, previous
   
-results = Classifer.new(test.historic_data, 'SQQQ').trained_examples
+  print "Waiting for historic data "
+  while hd.historic_data.keys.size < ndx10.size + 1
+    print '.' ; sleep 1
+  end
+  puts
+  hd
+end
+
+train = request_historic_data 2, 3
+knn = KNearestNeighbours.new Classifer.new('SQQQ', train.historic_data).
+  trained_examples
+
+test = request_historic_data 1, 1
+results = Classifer.new('SQQQ', test.historic_data).trained_examples
 
 positions = results.inject([]) do |positions, result|
   classification = result[:classification]
@@ -37,7 +35,7 @@ positions = results.inject([]) do |positions, result|
   positions << [classification, test_classification]
 end
 
-positions_taken = errors.find_all &:last
+positions_taken = positions.find_all &:last
 missed_positions = positions - positions_taken
 correct_positions = positions_taken.find_all {|e| e.first == e.last }.size
 incorrect_positions = positions_taken.size - correct_positions
