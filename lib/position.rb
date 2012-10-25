03a4e6b067443c378f57f5ca9bca39353f956613
Keith McDonnell <keith@dancingtext.com>
2012-10-25 12:34:59 +0100

    Preparing to move out to Account, TradeLog

--- lib/position.rb
+++ lib/position.rb
@@ -1,10 +1,11 @@
 # TODO: move Gateway functionality to Account
 class Position < Gateway
+  MINIMUM_BALANCE, ACCOUNT_RISK = 25_000, 0.005
   attr_accessor :entry, :account_balance, :profitability
   include Observable
   
   def initialize
-    super() # FIXME: pass in a/c code from config & environment
+    super() # TODO: pass in a/c code from config & environment
     client_socket.reqAccountUpdates true, 'DU125081'
   end
   
@@ -20,15 +21,23 @@ class Position < Gateway
   end
   
   def size
-    10_000 # fixed for evaluation
+    10_000 # TODO: change to fixed fraction
   end
   
   def daily_drawdown_tolerable?
-    profitability > -(account_balance * 0.005)
+    account_daily_drawdown_tolerable? && strategy_daily_drawdown_tolerable?
+  end
+  
+  def account_daily_drawdown_tolerable?
+    profitability > -(account_balance * ACCOUNT_RISK)
+  end
+  
+  def strategy_daily_drawdown_tolerable?
+    true
   end
   
   def balance_sufficient_for_day_trading?
-    account_balance > 25000
+    account_balance > MINIMUM_BALANCE
   end
   
   def updateAccountValue(key, value, currency, accountName)
