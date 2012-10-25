0e4a6a7ce4dc8f874e44766ae5155dce9dfe82cd
Keith McDonnell <keith@dancingtext.com>
2012-10-25 11:33:24 +0100

    Currently tradeable issues

--- bin/backtest.rb
+++ bin/backtest.rb
@@ -5,15 +5,12 @@ require 'backtest'
 HOLDINGS = { # TODO: move to yaml
    # Falsify GDX using %w[ QQQ TLT VXX UNG ],
   'GDX' => %w[ ABX GG NEM AUY SLW AU GOLD AEM KGC EGO GFI ],
-  'QQQ' => %w[ AAPL MSFT GOOG ORCL INTC AMZN QCOM CSCO CMCSA AMGN ],
-  'SMH' => %w[ INTC TSM TXN BRCM ARMH ASML AMAT ADI ALTR XLNX ],
+  'QLD' => %w[ AAPL MSFT GOOG ORCL INTC AMZN QCOM CSCO CMCSA AMGN ],
   'SDS' => %w[ XLY XLP XLE XLF XLV XLI XLB XLK XLU ],
-  'XLB' => %w[ MON DD FCX DOW PX NEM LYB PPG ECL APD ],
+  'VXX' => %w[ SPY XIV ],
   'XLE' => %w[ XOM CVX SLB OXY COP NOV APC APA HAL EOG ],
   'XME' => %w[ CNX CMP BTU ANR ACI WLT ANV CDE X HL CLD ]
 }
-
-%w[ QID QLD ].each {|ndx| HOLDINGS[ndx] = HOLDINGS['QQQ'].dup }
 
 # Usage ./bin/backtest.rb QQQ
 index = ARGV.first
