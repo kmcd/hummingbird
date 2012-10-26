670d988bf5012e79d6161b21ecf9f6a3d518bcc1
Keith McDonnell <keith@dancingtext.com>
2012-10-26 14:15:18 +0100

    Fix: future leak - only classify the *current* bar & *test* against the next

--- lib/example.rb
+++ lib/example.rb
@@ -1,14 +1,12 @@
 class Example
-  attr_reader :current_bar, :previous_bar, :entry, :exit
+  attr_reader :current_bar, :entry, :exit
   
-  def initialize(current_bar, previous_bar, entry=0.05, exit=0.02)
-    @current_bar, @previous_bar = current_bar, previous_bar
+  def initialize(current_bar, entry=0.05, exit=0.02)
+    @current_bar = current_bar
     @entry, @exit = entry, exit
   end
   
   def classification
-    return unless current_bar && previous_bar
-    return if previous_bar.empty?
     return :long if long?
     :short if short?
   end
@@ -22,7 +20,10 @@ class Example
   end
   
   def change(ohlc)
-    (BigDecimal.new(current_bar[ohlc].to_s) - BigDecimal.new(
-      previous_bar[ohlc].to_s) ).to_f
+    (price(ohlc) - price(:open)).to_f
+  end
+  
+  def price(ohlc)
+    BigDecimal.new current_bar[ohlc].to_s
   end
 end
